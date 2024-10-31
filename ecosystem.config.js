3b718bff21c19537f007d406a284b09922793f66
ilssn <ilssn@adswave.co>
2024-10-31 20:04:50 +0800

    upd: change deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
+//!!You need to close standalone output on next.config.mjs first
 module.exports = {
   apps: [
     {
-      name: 'AI-Restoration',
-      script: './node_modules/next/dist/bin/next',
-      args: 'start -p 3015', //3005是我们希望nextjs程序运行的端口号，你也可以修改为其他端口号
+      name: "302_Photo_Restoration",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p 3000",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
     },
   ],
